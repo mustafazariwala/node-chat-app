fd5b99b49c5c42a57919ce572ce07ccf125a341f
Mustafa Zariwala <mustafazariwala5253@gmail.com>
2017-09-03 23:10:33 +0500

    coomit

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
+var server = http.createServer(app);
+
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Listening on ${port}`)
 });
