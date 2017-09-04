d58b2eeb34b33d01e1342ad832d1cad87e362d37
Mustafa Zariwala <mustafazariwala5253@gmail.com>
2017-09-04 11:02:26 +0500

    newMessage

--- server/server.js
+++ server/server.js
@@ -1,14 +1,35 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const http = require('http');
-const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
+
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
+var app = express();
 var server = http.createServer(app);
+var io = socketIO(server);
+
 
 app.use(express.static(publicPath));
+
+io.on('connection',(socket) => {
+  console.log('New User Connected')
+
+
+  socket.on('createMessage',(message) => {
+    console.log('new Message', message);
+    io.emit('newMessage', {
+      from : message.from,
+      text: message.text,
+      createAt: new Date().getTime()
+    })
+  })
+
+socket.on('disconnect', () => {
+  console.log('Disonnected')
+})
+})
 
 server.listen(port, () => {
   console.log(`Listening on ${port}`)
